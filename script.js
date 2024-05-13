09e6c0cd5eb168edf05bec3992043ef9b4b70432
Momina Din <momina@mominas-mbp-2.mynetworksettings.com>
2024-05-13 15:46:17 -0400

    first commit - includes a basic homepage for docs

---
+++ script.js
@@ -0,0 +1,17 @@
+/* variable one = 10
+variable two = 40
+variable three = 39
+pop-up ("string" + varaible one + variable two + variable three)
+*/
+
+// Using addition to output value of 10
+const one = 5 + 5;
+
+// Using addition to output value of 40
+const two = 8 * 5;
+
+// Using subtraction to output value of 39
+const three = 50 - 11;
+
+
+alert("You have this message because you have been chosen to open an important vault. Here is the secret combination:" + " " + one + " " + two + " " + three);
